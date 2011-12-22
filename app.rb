dbdb2f0cc5b648d7c78df3fd733e2e8041527eb6
Kingdon Barrett <yebyen@nerdland.info>
2011-12-22 18:35:35 -0500

    using nokogiri and patron

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ class App < Sinatra::Base
   require 'views/hello'
   require 'views/document'
   require 'sqlite3'
+  require 'patron'
+  require 'nokogiri'
 
   def initialize
     super
@@ -36,11 +38,17 @@ class App < Sinatra::Base
     Views::Hello::have ([params[:foo], params[:bar], params[:baz]])
     mustache :hello
   end
-  get '/document*' do
-    row = @db.execute(
-      "select * from to_document" )
+  get '/document*' do |path|
+	#row=[{"table_schema" => path}]
+	sess=Patron::Session.new
+	sess.base_url="http://sellercentral.amazon.com/"
+	response=sess.get path
+	html_doc=Nokogiri::HTML(response.body)
+	form=html_doc.css("form")
+	row=[{"url" => html_doc.title, "body" => form}]
+	
     Views::Document::have ( row )
-    mustache :document
+    mustache :document, :layout => false
   end
 
   get '/nolayout' do
